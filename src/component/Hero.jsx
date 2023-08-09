298165638eb19bd73827445400a2609529e44ad5
surazgaundel <gaundelsuraz99@gmail.com>
2023-08-09 15:34:47 +0530

    Initial service component

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import AboutImage from '../assets/about.jpeg';
+import HeroImage from '../assets/hero.jpeg';
 export default function Hero() {
   return (
-    <div className="h-[70vh] flex flex-row items-center justify-center">
-        <div>
-            <h1>&quot;Unwind your Mind,<br/> Embrace Serenity&quot;</h1>
-            <p> Seek out furniture that beckons you to sink in and indulge in pure bliss. Picture yourself lounging on plush sofas or nestling into the embrace of an armchair with delightfully cushioned seating. Enhance your comfort zone by opting for a peaceful art that allows you to fully unwind or a chaise lounge that invites endless relaxation possibilities</p>
-            <Link to='/products' className=' px-2 py-1 mt-5 rounded-md text-white transition ease-in-out delay-150 bg-darkRed hover:-translate-y-1 hover:scale-110 hover:bg-green duration-300'>Explore</Link>
-        </div>
+    <div className="h-[70vh] flex flex-row items-center justify-around">
+        <article className="p-1 w-[80%] md:w-[60%]">
+            <h1 className='text-2xl md:text-4xl lg:text-5xl font-bold tracking-wider'>&quot;Unwind your Mind,<br/> Embrace Serenity&quot;</h1>
+            <p className='text-sm md:text-xl leading-8 my-10'> Seek out furniture that beckons you to sink in and indulge in pure bliss. Picture yourself lounging on plush sofas or nestling into the embrace of an armchair with delightfully cushioned seating. Enhance your comfort zone by opting for a peaceful art that allows you to fully unwind or a chaise lounge that invites endless relaxation possibilities</p>
+            <Link to='/products' className=' px-3 text-xl py-1 rounded-md text-white transition ease-in-out delay-150 bg-darkRed hover:-translate-y-1 hover:scale-110 hover:bg-green duration-300'>Shop Now</Link>
+        </article>
+        <article className='hidden relative md:block w-[20%] mr-3'>
+            <img src={HeroImage} className='absolute bottom-0 rounded-md -left-10 h-32 border-green border-2' alt='hero image'/>
+            <img src={AboutImage} className='rounded-md border-green border-2' alt='about image'/>
+        </article>
     </div>
   )
 }
