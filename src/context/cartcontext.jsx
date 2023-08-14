fb549812b6e9b879eee635e2d933714c00205588
surazgaundel <gaundelsuraz99@gmail.com>
2023-08-14 16:30:03 +0530

    added local storage funcitonality

--- src/context/cartcontext.jsx
+++ src/context/cartcontext.jsx
@@ -1,8 +1,19 @@
-import { createContext, useReducer,useContext } from "react"
+import { createContext, useReducer,useContext, useEffect } from "react"
 import reducer from '../reducers/cart_reducer';
 
+
+const getLocalStorage=()=>{
+    let cart=localStorage.getItem('cart');
+    if(cart){
+        return JSON.parse(localStorage.getItem('cart'))
+    }
+    else{
+        return [];
+    }
+}
+
 const initialState ={
-    cart:[],
+    cart:getLocalStorage(),
     totalItems:0,
     totalAmount:0,
     shippingFee:230,
@@ -14,11 +25,27 @@ export const CartProvider =({children})=>{
     const [state,dispatch]=useReducer(reducer,initialState);
 
     //add to cart
-    const  addToCart =(id,color,amount,product)=>{
+    const  addToCart=(id,color,amount,product)=>{
         dispatch({type:'ADD_TO_CART',payload:{id,color,amount,product}});
     }
+
+    //remove item
+    const removeItem=(id)=>{
+        // dispatch({type:})
+    }
+    const toggleAmount=(id,value)=>{
+        // dispatch({type:})
+    }
+    const clearCart=()=>{
+        // dispatch({type:})
+    }
+
+    useEffect(()=>{
+        localStorage.setItem('cart',JSON.stringify(state.cart));    
+    },[state.cart])
+
     return(
-        <CartContext.Provider value={{...state,addToCart}}>
+        <CartContext.Provider value={{...state,addToCart,removeItem,toggleAmount,clearCart}}>
             {children}
         </CartContext.Provider>
     )
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
-
+import { useCartContext } from '../context/cartcontext'
+import { Link } from 'react-router-dom';
+import { PageHero } from '../component';
 export default function CartPage() {
+  const {cart}=useCartContext();
+  if(cart.length<1){
+    return(
+      <div className='container flex flex-col justify-center items-center'>
+        <h1 className='text-xl'>Your cart is empty.</h1>
+        <Link to='/products' className='hover-btn text-base'>All products</Link>
+      </div>
+    )
+  }
   return (
-    <div>CartPage</div>
+    <div>
+      <PageHero title='cart'/>
+      <div className='container'>
+
+      </div>
+    </div>
   )
 }
